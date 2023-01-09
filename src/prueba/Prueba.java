f4b732537207f0b9d363a7e8009f3c01a3b9db94
carlo <carlo@CARLOSALD>
2023-01-09 10:53:39 +0100

    Segundo commit

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -4,8 +4,6 @@ public class Prueba {
 	public static void main(String[] args) {
 		
 		System.out.println("Hola mundo");
-		System.out.println("Este programa va a empezar");
-		System.out.println("Este programa va a terminar");
-		System.out.println("Ádios");
+		
 	}
 }
